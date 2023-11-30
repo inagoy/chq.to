2a83d06af141725a07f3961f44f10c5d90eff879
Iñaki Goyeneche <igoyeneche.98@gmail.com>
2023-11-30 13:08:45 -0300

    [feat] devise gem login-registration and user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configuer_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configuer_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name])
+  end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  with_options presence: true, length: { minimun: 3, maximum: 100 } do
+    validates :username, uniqueness: true, format: { with: /\A[a-zA-Z0-9]+\z/ }
+    validates :first_name, :last_name, format: { with: /\A[a-zA-ZñáéíóúÁÉÍÓÚüÜ]+\z/ }
+  end
+
+  has_many :links
+
+end
